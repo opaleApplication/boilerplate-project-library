1cb6ff0d3e07c3043e5873ddb4ee14281a4ae8d8
J. RCD <infos@opale-application.com>
2026-03-01 12:06:13 +0100

    feat: implement personal library api with mongodb and functional tests

--- routes/api.js
+++ routes/api.js
@@ -8,75 +8,253 @@
 
 'use strict';
 
-module.exports = function (app) {
-  const books = [];
+const { MongoClient, ObjectId } = require('mongodb');
 
-  const createId = () => {
-    return (
-      Date.now().toString(16) + Math.random().toString(16).slice(2)
-    );
+module.exports = function (app) {
+  const useMemoryStore = !process.env.DB;
+  const memoryBooks = [];
+  let booksCollectionPromise;
+
+  const createMemoryId = function () {
+    return Date.now().toString(16) + Math.random().toString(16).slice(2);
   };
 
-  app.route('/api/books')
-    .get(function (req, res){
-      //response will be array of book objects
-      //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
-      const booksWithCommentCount = books.map(function (book) {
+  const normalizeBook = function (book) {
+    return {
+      _id: String(book._id),
+      title: book.title,
+      comments: Array.isArray(book.comments) ? book.comments : []
+    };
+  };
+
+  const getCollection = function () {
+    if (!booksCollectionPromise) {
+      const client = new MongoClient(process.env.DB);
+      booksCollectionPromise = client.connect()
+        .then(function () {
+          return client.db().collection('books');
+        })
+        .catch(function (error) {
+          booksCollectionPromise = null;
+          throw error;
+        });
+    }
+
+    return booksCollectionPromise;
+  };
+
+  const getObjectId = function (bookid) {
+    if (!ObjectId.isValid(bookid)) {
+      return null;
+    }
+
+    return new ObjectId(bookid);
+  };
+
+  const listBooks = async function () {
+    if (useMemoryStore) {
+      return memoryBooks.map(function (book) {
         return {
           _id: book._id,
           title: book.title,
           commentcount: book.comments.length
         };
       });
+    }
 
-      res.json(booksWithCommentCount);
+    const booksCollection = await getCollection();
+    const books = await booksCollection
+      .find({}, { projection: { title: 1, comments: 1 } })
+      .toArray();
+
+    return books.map(function (book) {
+      return {
+        _id: String(book._id),
+        title: book.title,
+        commentcount: Array.isArray(book.comments) ? book.comments.length : 0
+      };
+    });
+  };
+
+  const createBook = async function (title) {
+    if (useMemoryStore) {
+      const book = {
+        _id: createMemoryId(),
+        title: title,
+        comments: []
+      };
+
+      memoryBooks.push(book);
+
+      return {
+        _id: book._id,
+        title: book.title
+      };
+    }
+
+    const booksCollection = await getCollection();
+    const result = await booksCollection.insertOne({
+      title: title,
+      comments: []
+    });
+
+    return {
+      _id: String(result.insertedId),
+      title: title
+    };
+  };
+
+  const deleteAllBooks = async function () {
+    if (useMemoryStore) {
+      memoryBooks.length = 0;
+      return;
+    }
+
+    const booksCollection = await getCollection();
+    await booksCollection.deleteMany({});
+  };
+
+  const getBookById = async function (bookid) {
+    if (useMemoryStore) {
+      const book = memoryBooks.find(function (entry) {
+        return entry._id === bookid;
+      });
+
+      if (!book) {
+        return null;
+      }
+
+      return normalizeBook(book);
+    }
+
+    const objectId = getObjectId(bookid);
+    if (!objectId) {
+      return null;
+    }
+
+    const booksCollection = await getCollection();
+    const book = await booksCollection.findOne({ _id: objectId });
+
+    if (!book) {
+      return null;
+    }
+
+    return normalizeBook(book);
+  };
+
+  const addComment = async function (bookid, comment) {
+    if (useMemoryStore) {
+      const book = memoryBooks.find(function (entry) {
+        return entry._id === bookid;
+      });
+
+      if (!book) {
+        return null;
+      }
+
+      book.comments.push(comment);
+      return normalizeBook(book);
+    }
+
+    const objectId = getObjectId(bookid);
+    if (!objectId) {
+      return null;
+    }
+
+    const booksCollection = await getCollection();
+    const result = await booksCollection.findOneAndUpdate(
+      { _id: objectId },
+      { $push: { comments: comment } },
+      { returnDocument: 'after' }
+    );
+
+    if (!result.value) {
+      return null;
+    }
+
+    return normalizeBook(result.value);
+  };
+
+  const deleteBook = async function (bookid) {
+    if (useMemoryStore) {
+      const index = memoryBooks.findIndex(function (entry) {
+        return entry._id === bookid;
+      });
+
+      if (index === -1) {
+        return false;
+      }
+
+      memoryBooks.splice(index, 1);
+      return true;
+    }
+
+    const objectId = getObjectId(bookid);
+    if (!objectId) {
+      return false;
+    }
+
+    const booksCollection = await getCollection();
+    const result = await booksCollection.deleteOne({ _id: objectId });
+    return result.deletedCount === 1;
+  };
+
+  app.route('/api/books')
+    .get(async function (req, res){
+      //response will be array of book objects
+      //json res format: [{"_id": bookid, "title": book_title, "commentcount": num_of_comments },...]
+      try {
+        const books = await listBooks();
+        res.json(books);
+      } catch (error) {
+        res.status(500).send('internal server error');
+      }
     })
     
-    .post(function (req, res){
+    .post(async function (req, res){
       let title = req.body.title;
       //response will contain new book object including atleast _id and title
       if (!title) {
         return res.send('missing required field title');
       }
 
-      const newBook = {
-        _id: createId(),
-        title: title,
-        comments: []
-      };
-
-      books.push(newBook);
-
-      return res.json({
-        _id: newBook._id,
-        title: newBook.title
-      });
+      try {
+        const book = await createBook(title);
+        return res.json(book);
+      } catch (error) {
+        return res.status(500).send('internal server error');
+      }
     })
     
-    .delete(function(req, res){
+    .delete(async function(req, res){
       //if successful response will be 'complete delete successful'
-      books.length = 0;
-      res.send('complete delete successful');
+      try {
+        await deleteAllBooks();
+        res.send('complete delete successful');
+      } catch (error) {
+        res.status(500).send('internal server error');
+      }
     });
 
 
 
   app.route('/api/books/:id')
-    .get(function (req, res){
+    .get(async function (req, res){
       let bookid = req.params.id;
       //json res format: {"_id": bookid, "title": book_title, "comments": [comment,comment,...]}
-      const book = books.find(function (entry) {
-        return entry._id === bookid;
-      });
+      try {
+        const book = await getBookById(bookid);
+        if (!book) {
+          return res.send('no book exists');
+        }
 
-      if (!book) {
-        return res.send('no book exists');
+        return res.json(book);
+      } catch (error) {
+        return res.status(500).send('internal server error');
       }
-
-      return res.json(book);
     })
     
-    .post(function(req, res){
+    .post(async function(req, res){
       let bookid = req.params.id;
       let comment = req.body.comment;
       //json res format same as .get
@@ -84,31 +262,31 @@ module.exports = function (app) {
         return res.send('missing required field comment');
       }
 
-      const book = books.find(function (entry) {
-        return entry._id === bookid;
-      });
+      try {
+        const book = await addComment(bookid, comment);
+        if (!book) {
+          return res.send('no book exists');
+        }
 
-      if (!book) {
-        return res.send('no book exists');
+        return res.json(book);
+      } catch (error) {
+        return res.status(500).send('internal server error');
       }
-
-      book.comments.push(comment);
-      return res.json(book);
     })
     
-    .delete(function(req, res){
+    .delete(async function(req, res){
       let bookid = req.params.id;
       //if successful response will be 'delete successful'
-      const bookIndex = books.findIndex(function (entry) {
-        return entry._id === bookid;
-      });
+      try {
+        const deleted = await deleteBook(bookid);
+        if (!deleted) {
+          return res.send('no book exists');
+        }
 
-      if (bookIndex === -1) {
-        return res.send('no book exists');
+        return res.send('delete successful');
+      } catch (error) {
+        return res.status(500).send('internal server error');
       }
-
-      books.splice(bookIndex, 1);
-      return res.send('delete successful');
     });
   
 };
